dba0af9d7b508bc2b5bfab294a3ffc007af7c60e
devyousefm <devyousefm@gmail.com>
2024-05-05 09:07:55 +0300

    -

--- resources/views/article.blade.php
+++ resources/views/article.blade.php
@@ -66,11 +66,10 @@
                                 </div>
                             </div>
                             <div class="readmore mb-0">
-                                {{ route('article.show', $item->translations[0]->slug) }}
-                                {{-- <a class="btn-two" href='{{ route('article.show', $item->translations[0]->slug) }}'><span
-                                        class="icon-arrow"></span><span>@lang('site.know_more')</span></a> --}}
 
-                                {{ $item->translations[0]->slug }}
+                                <a class="btn-two"
+                                    href="{{ route('article.show', ['slug' => $item->translations[0]->slug, 'service_slug' => $service_slug]) }}"><span
+                                        class="icon-arrow"></span><span>@lang('site.know_more')</span></a>
                             </div>
                         </div>
                     </div>
